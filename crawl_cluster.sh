70886477dd5380076440834bb7ad13eaaec9e691
Ralf Schmidt <Ralf.Schmidt@f-i-ts.de>
2024-07-17 15:54:38 +0200

    erste Schleife zur Abfrage

--- crawl_cluster.sh
+++ crawl_cluster.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 #Skript zum Sammeln von Daten eines gegebenen Clusters
+
+#nach Anmeldung per cloudctl login Cluster abfragen
+for MYCLUSTER in $(cloudctl cluster list --tenant fttc | awk '{print $4}' | grep -v NAME); do echo ${MYCLUSTER}; MYCLUSTERID=$(cloudctl cluster list --tenant fttc | grep -w ${MYCLUSTER} | awk '{print $1}'); echo ${MYCLUSTERID}; cloudctl cluster describe ${MYCLUSTERID} -o json > ${MYCLUSTER}-result.json; done
