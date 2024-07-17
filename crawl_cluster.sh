adce30ea4cbc9dcd7c03bfaf05a4e52a5b45aa89
Ralf Schmidt <Ralf.Schmidt@f-i-ts.de>
2024-07-17 15:59:01 +0200

    zweiter Schritt

--- crawl_cluster.sh
+++ crawl_cluster.sh
@@ -2,5 +2,10 @@
 
 #Skript zum Sammeln von Daten eines gegebenen Clusters
 
+mkdir results
+rm -rf results/*
+
 #nach Anmeldung per cloudctl login Cluster abfragen
-for MYCLUSTER in $(cloudctl cluster list --tenant fttc | awk '{print $4}' | grep -v NAME); do echo ${MYCLUSTER}; MYCLUSTERID=$(cloudctl cluster list --tenant fttc | grep -w ${MYCLUSTER} | awk '{print $1}'); echo ${MYCLUSTERID}; cloudctl cluster describe ${MYCLUSTERID} -o json > ${MYCLUSTER}-result.json; done
+for MYCLUSTER in $(cloudctl cluster list --tenant fttc | awk '{print $4}' | grep -v NAME); do echo ${MYCLUSTER}; MYCLUSTERID=$(cloudctl cluster list --tenant fttc | grep -w ${MYCLUSTER} | awk '{print $1}'); echo ${MYCLUSTERID}; cloudctl cluster describe ${MYCLUSTERID} -o json > results/${MYCLUSTER}-result.json; done
+
+exit 0
