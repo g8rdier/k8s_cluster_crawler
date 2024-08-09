408a7e6c884fd8ec008be69b3c592f78b4b60838
Kobilarov, Gregor <gregor.kobilarov@f-i-ts.de>
2024-08-09 13:10:47 +0000

    Anpassung an Lint-Warnungen SC2086 (Doppelte Anführungszeichen) & SC2013 (Um Zeilen statt Wörter zu lesen, in eine 'while read'-Schleife umleiten)

--- scripts/crawl_clusters.sh
+++ scripts/crawl_clusters.sh
@@ -10,10 +10,10 @@ FORCE_REBUILD=1
 # Funktion zum Erstellen eines Verzeichnisses, falls es nicht existiert
 create_directory() {
     locDir="${1}"
-    if [ ! -d ${locDir} ]; then
-        mkdir -p ${locDir} > /dev/null
+    if [ ! -d "${locDir}" ]; then
+        mkdir -p "${locDir}" > /dev/null
     fi
-    if [ ! -d ${locDir} ]; then
+    if [ ! -d "${locDir}" ]; then
         echo "error: failed to create directory '${locDir}'"
         return 1
     fi
@@ -23,7 +23,7 @@ create_directory() {
 # Funktion zum Erstellen eines leeren Verzeichnisses (falls vorhanden, wird es entfernt, dann neu erstellt)
 create_empty_directory() {
     locDir="${1}"
-    rm -rf ${locDir} > /dev/null 2>&1
+    rm -rf "${locDir}" > /dev/null 2>&1
     create_directory "${locDir}"
     return $?
 }
@@ -61,9 +61,9 @@ INFO_CACHE="${TMPS_DIR}/info_cache_${DAYSTAMP}"
 
 if [ "${FORCE_REBUILD}" == "1" ]; then
     echo "info: environment variable 'FORCE_REBUILD' is set to 1, refreshing all cached cluster information"
-    rm -rf ${INFO_CACHE} > /dev/null 2>&1
+    rm -rf "${INFO_CACHE}" > /dev/null 2>&1
 
-    if [ -d ${INFO_CACHE} ]; then
+    if [ -d "${INFO_CACHE}" ]; then
         echo "error: failed to clear cache '${INFO_CACHE}'"
         exit 1
     fi
@@ -71,29 +71,29 @@ else
     echo "info: environment variable 'FORCE_REBUILD' not set to 1, using cached cluster information"
 fi
 
-if [ ! -d ${INFO_CACHE} ]; then
+if [ ! -d "${INFO_CACHE}" ]; then
     # Möglicherweise ein neuer Tag, daher alle alten info_cache Verzeichnisse löschen
-    rm -rf ${TMPS_DIR}/info_cache_* > /dev/null 2>&1
-    create_empty_directory ${INFO_CACHE}
+    rm -rf "${TMPS_DIR}/info_cache_*" > /dev/null 2>&1
+    create_empty_directory "${INFO_CACHE}"
 fi
 
 # Funktion zum Protokollieren von Fehlern und Debugging-Informationen
 debug_crawler_error() {
     pwd
     ls -al
-    ls -al ${INFO_CACHE}
+    ls -al "${INFO_CACHE}"
 }
 
 # IP Scraper: Abrufen und Speichern der IP-Adressen aller Cluster
 CLSTR_IPS="${INFO_CACHE}/cluster_ips.json"
-if [ ! -s ${CLSTR_IPS} ]; then
-    if ! cloudctl ip list -o json > ${CLSTR_IPS}; then
+if [ ! -s "${CLSTR_IPS}" ]; then
+    if ! cloudctl ip list -o json > "${CLSTR_IPS}"; then
         echo "error: failed to execute 'cloudctl ip list -o json' command"
         debug_crawler_error
         exit 1
     fi
 
-    if [ ! -s ${CLSTR_IPS} ]; then
+    if [ ! -s "${CLSTR_IPS}" ]; then
         echo "error: 'cloudctl ip list' did not produce output in '${CLSTR_IPS}'"
         debug_crawler_error
         exit 1
@@ -102,14 +102,14 @@ fi
 
 # NAMEID Scraper: Abrufen und Speichern der Name-ID-Zuordnung aller Cluster
 NAMEID_MAP="${INFO_CACHE}/name_id.map"
-if [ ! -s ${NAMEID_MAP} ]; then
-    > ${NAMEID_MAP}
+if [ ! -s "${NAMEID_MAP}" ]; then
+    > "${NAMEID_MAP}"
 
     echo "debug: unsere cluster sind '${UNSERE_CLUSTER}'"
     for tnt in ${UNSERE_CLUSTER}; do
         echo "debug: tenant is ${tnt}"
         # Überprüfen, ob Cluster für jeden Tenant aufgelistet sind und sie zur Map hinzufügen
-        if ! cloudctl cluster list --tenant ${tnt} | grep -v "NAME" | awk '{ print $4";"$1 }' >> ${NAMEID_MAP}; then
+        if ! cloudctl cluster list --tenant "${tnt}" | grep -v "NAME" | awk '{ print $4";"$1 }' >> "${NAMEID_MAP}"; then
             echo "error: failed to list clusters for tenant '${tnt}'"
             debug_crawler_error
             exit 1
@@ -117,7 +117,7 @@ if [ ! -s ${NAMEID_MAP} ]; then
     done
 
     # Letzte Überprüfung, ob die name_id.map Datei gefüllt ist
-    if [ ! -s ${NAMEID_MAP} ]; then
+    if [ ! -s "${NAMEID_MAP}" ]; then
         echo "error: failed to create / fill '${NAMEID_MAP}'"
         debug_crawler_error
         exit 1
@@ -125,30 +125,30 @@ if [ ! -s ${NAMEID_MAP} ]; then
 fi
 
 # Cluster Describer: Abrufen und Speichern detaillierter Informationen für jeden Cluster
-for line in $(cat ${NAMEID_MAP}); do
-    CLSTRNM=$(echo ${line} | cut -f 1 -d ";")
-    CLSTRID=$(echo ${line} | cut -f 2 -d ";")
+while IFS= read -r line; do
+    CLSTRNM=$(echo "${line}" | cut -f 1 -d ";")
+    CLSTRID=$(echo "${line}" | cut -f 2 -d ";")
     echo "debug: will describe ${CLSTRNM} with ${CLSTRID}"
 
     CLSTR_INFO="${INFO_CACHE}/${CLSTRNM}_describe.json"
-    if [ ! -s ${CLSTR_INFO} ]; then
-        if ! cloudctl cluster describe ${CLSTRID} -o json > ${CLSTR_INFO}; then
+    if [ ! -s "${CLSTR_INFO}" ]; then
+        if ! cloudctl cluster describe "${CLSTRID}" -o json > "${CLSTR_INFO}"; then
             echo "error: failed to describe cluster '${CLSTRNM}' with ID '${CLSTRID}'"
             debug_crawler_error
             exit 1
         fi
     fi
 
-    if [ ! -s ${CLSTR_INFO} ]; then
+    if [ ! -s "${CLSTR_INFO}" ]; then
         echo "error: failed to create / fill '${CLSTR_INFO}'"
         debug_crawler_error
         exit 1
     fi
-done
+done < "${NAMEID_MAP}"
 
 # Kubernetes Data Collector: Abrufen und Speichern von Kubernetes-Informationen (Pods und Ingress) für jeden Cluster
-for line in $(cat ${NAMEID_MAP}); do
-    CLSTRNM=$(echo ${line} | cut -f 1 -d ";")
+while IFS= read -r line; do
+    CLSTRNM=$(echo "${line}" | cut -f 1 -d ";")
     echo "debug: will kubectl cluster content for ${CLSTRNM}"
 
     # Dateipfade für das Speichern von Clusterinformationen definieren
@@ -175,26 +175,26 @@ for line in $(cat ${NAMEID_MAP}); do
         debug_crawler_error
         exit 1
     fi
-done
+done < "${NAMEID_MAP}"
 
 # Doppelte Überprüfung, ob alle Cluster aus name_id.map verarbeitet wurden
-for line in $(cat ${NAMEID_MAP}); do
-    CLSTRNM=$(echo ${line} | cut -f 1 -d ";")
-    CLSTR_INFO="${TMPS_DIR}/${CLSTRNM}_describe.json"
+while IFS= read -r line; do
+    CLSTRNM=$(echo "${line}" | cut -f 1 -d ";")
+    CLSTR_INFO="${INFO_CACHE}/${CLSTRNM}_describe.json"
     
-    if [ ! -s ${CLSTR_INFO} ]; then
+    if [ ! -s "${CLSTR_INFO}" ]; then
         echo "Fehler: Cluster '${CLSTRNM}' wurde nicht korrekt verarbeitet"
         debug_crawler_error
         ALL_CLUSTERS_PROCESSED_SUCCESSFULLY=false
     fi
-done
+done < "${NAMEID_MAP}"
 
 # Cleanup-Funktion für das Entfernen der Dateien, wenn alle Cluster erfolgreich verarbeitet wurden
 cleanup() {
-    if [ "$ALL_CLUSTERS_PROCESSED_SUCCESSFULLY" = true ]; then
+    if [ "${ALL_CLUSTERS_PROCESSED_SUCCESSFULLY}" = true ]; then
         echo "Info: Alle Cluster wurden erfolgreich verarbeitet, name_id.map und cluster_ips.json werden entfernt"
-        rm -f ${NAMEID_MAP}
-        rm -f ${CLSTR_IPS}
+        rm -f "${NAMEID_MAP}"
+        rm -f "${CLSTR_IPS}"
     else
         echo "Info: Einige Cluster wurden nicht erfolgreich verarbeitet, name_id.map und cluster_ips.json bleiben zur weiteren Überprüfung erhalten."
     fi
