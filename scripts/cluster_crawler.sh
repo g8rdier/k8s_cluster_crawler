cbb35ae29e5f6c8c9d13ad8bdf8a43f083aa84e3
Kobilarov, Gregor <gregor.kobilarov@f-i-ts.de>
2024-09-11 12:34:01 +0000

    git token & repo correct config

--- scripts/cluster_crawler.sh
+++ scripts/cluster_crawler.sh
@@ -133,9 +133,9 @@ while IFS=";" read -r CLSTRNM _CLSTRID; do
 done < "${NAMEID_MAP}"
 
 # Git-Token und Repository konfigurieren
-GIT_USER="gitlab+deploy-token-<ID>"
-GIT_TOKEN="${rnwer}"  # Env-Variable für den Token
-GIT_REPO_URL="https://"rnwer":${PUSHER}@gitlab.com/devops-services/toolchain/docs.git"
+GIT_USER="gitlab+deploy-token-90"
+GIT_TOKEN="${PUSH_BOM_PAGES}"  # Env-Variable für den Token
+GIT_REPO_URL="https://"rnwer":${PUSH_BOM_PAGES}@gitlab.com/devops-services/toolchain/docs.git"
 
 
 # Klonen des Repositories in ein temporäres Verzeichnis
