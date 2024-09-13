39c894bd52d3b97003f97c584e710ccf4467fa1e
Kobilarov, Gregor <gregor.kobilarov@f-i-ts.de>
2024-09-13 10:45:17 +0000

    Update cluster_crawler.sh

--- scripts/cluster_crawler.sh
+++ scripts/cluster_crawler.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
 
-# set -x  # Debugging-Modus aktivieren (gibt jeden Befehl aus)
-
-# Standard-Logging-Level
+# Standard logging level
 DETAILLIERTES_LOGGING=false
 
-# Argumente der Befehlszeile parsen
+# Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -dl) DETAILLIERTES_LOGGING=true ;;  # Detailliertes Logging aktivieren
-        *) echo "Unbekannter Parameter übergeben: $1"; exit 1 ;;
+        -dl) DETAILLIERTES_LOGGING=true ;;  # Enable detailed logging
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-# Funktion zum Loggen von Nachrichten (abhängig vom Logging-Flag)
+# Logging function (depending on the logging flag)
 log() {
     local level="$1"
     local message="$2"
@@ -25,48 +24,47 @@ log() {
     fi
 }
 
-# Skript zum Sammeln von Daten aller Cluster
-# shellcheck disable=SC2034
+# Script to collect data from all clusters
 UNSERE_CLUSTER="fttc ftctl"
 
-# Marker-Datei zur Verfolgung, ob das Skript bereits erfolgreich ausgeführt wurde
+# Marker file to track if the script has already run successfully
 MARKER_FILE="$(dirname "$0")/cluster_crawler_marker"
 
-# Überprüfen, ob FORCE_REBUILD gesetzt werden soll
+# Determine if FORCE_REBUILD should be set
 if [ -f "$MARKER_FILE" ]; then
     FORCE_REBUILD=0
 else
     FORCE_REBUILD=1
 fi
 
-# Funktion zum Erstellen eines Verzeichnisses, falls es nicht existiert
+# Function to create a directory if it doesn't exist
 create_directory() {
     locDir="${1}"
     if [ ! -d "${locDir}" ]; then
         mkdir -p "${locDir}" > /dev/null
     fi
     if [ ! -d "${locDir}" ]; then
-        log "ERROR" "Fehler beim Erstellen des Verzeichnisses '${locDir}'"
+        log "ERROR" "Error creating directory '${locDir}'"
         return 1
     fi
     return 0
 }
 
-# Funktion zum Setzen des Kubernetes-Kontexts für einen gegebenen Cluster
+# Function to set the Kubernetes context for a given cluster
 set_kube_context() {
     if kubectl config use-context "$1"; then
-        log "INFO" "Erfolgreich zu Kontext ${1} gewechselt"
+        log "INFO" "Successfully switched to context ${1}"
         return 0
     else
-        log "ERROR" "Fehler beim Wechseln zu Kontext ${1}"
+        log "ERROR" "Error switching to context ${1}"
         return 1
     fi
 }
 
-# temporäres Verzeichnis
+# Temporary directory
 DAYSTAMP="$(date +"%Y%m%d")"
 
-# Einrichten des Info-Cache-Verzeichnisses
+# Set up the info cache directory
 INFO_CACHE="info_cache_${DAYSTAMP}"
 
 if ! create_directory "${INFO_CACHE}"; then
@@ -74,45 +72,45 @@ if ! create_directory "${INFO_CACHE}"; then
 fi
 
 if [ "${FORCE_REBUILD}" == "1" ]; then
-    log "INFO" "Umgebungsvariable 'FORCE_REBUILD' ist auf 1 gesetzt, aktualisiere alle zwischengespeicherten Cluster-Informationen"
-    rm -rf "${INFO_CACHE:?}/*" > /dev/null 2>&1
+    log "INFO" "FORCE_REBUILD is set to 1, updating all cached cluster information"
+    rm -rf ${INFO_CACHE:?}/* > /dev/null 2>&1 || true
 else
-    log "INFO" "Umgebungsvariable 'FORCE_REBUILD' ist nicht auf 1 gesetzt, benutze zwischengespeicherte Cluster-Informationen"
+    log "INFO" "FORCE_REBUILD is not set to 1, using cached cluster information"
 fi
 
-# Funktion zum Protokollieren von Fehlern und Debugging-Informationen
+# Function for error logging and debugging
 debug_crawler_error() {
     pwd
     ls -al
     ls -al "${INFO_CACHE}"
 }
 
-# NAMEID Scraper: Abrufen und Speichern der Name-ID-Zuordnung aller Cluster
+# NAMEID Scraper: Retrieve and save name-ID mapping for all clusters
 NAMEID_MAP="scripts/docs/name_id.map"
 
 if [ -f "${NAMEID_MAP}" ]; then
-    log "INFO" "Statische name_id.map Datei gefunden. Verwende die Datei für die Cluster-Informationen."
+    log "INFO" "Static name_id.map file found. Using the file for cluster information."
 else
-    log "ERROR" "Statische name_id.map Datei nicht gefunden! Bitte überprüfen."
+    log "ERROR" "Static name_id.map file not found! Please check."
     debug_crawler_error
     exit 1
 fi
 
-# Kubernetes Data Collector: Abrufen und Speichern von Kubernetes-Informationen (Pods und Ingress) für jeden Cluster
+# Kubernetes Data Collector: Retrieve and save Kubernetes information (pods and ingress) for each cluster
 while IFS=";" read -r CLSTRNM _CLSTRID; do
-    log "DEBUG" "Hole kubectl Cluster-Inhalte für ${CLSTRNM}"
+    log "DEBUG" "Fetching kubectl cluster contents for ${CLSTRNM}"
 
-    # Dateipfade für das Speichern von Clusterinformationen definieren
+    # Define file paths for saving cluster information
     CLSTR_PODS="${INFO_CACHE}/${CLSTRNM}_pods.json"
     CLSTR_INGRESS="${INFO_CACHE}/${CLSTRNM}_ingress.json"
 
     if ! kubectl config get-contexts "${CLSTRNM}" &> /dev/null; then
-        log "WARNING" "Kein kube Kontext für Cluster '${CLSTRNM}' gefunden, überspringe..."
+        log "WARNING" "No kube context found for cluster '${CLSTRNM}', skipping..."
         continue
     fi
 
     if ! set_kube_context "${CLSTRNM}"; then
-        log "ERROR" "Fehler beim Setzen des kube Kontextes für Cluster '${CLSTRNM}'"
+        log "ERROR" "Error setting kube context for cluster '${CLSTRNM}'"
         debug_crawler_error
         exit 1
     fi
@@ -120,42 +118,64 @@ while IFS=";" read -r CLSTRNM _CLSTRID; do
     KUBE_VERSION=$(kubectl version --output=json | jq -r '.serverVersion.gitVersion')
 
     if ! kubectl get pods -A -o json | jq --arg k8s_version "$KUBE_VERSION" '.items |= map(. + {kubernetesVersion: $k8s_version})' > "${CLSTR_PODS}"; then
-        log "ERROR" "Fehler beim Abrufen der Pods für Cluster '${CLSTRNM}'"
+        log "ERROR" "Error fetching pods for cluster '${CLSTRNM}'"
         debug_crawler_error
         exit 1
+    else
+        log "INFO" "Pod data for cluster '${CLSTRNM}' saved successfully"
     fi
 
     if ! kubectl get ingress -A -o json > "${CLSTR_INGRESS}"; then
-        log "ERROR" "Fehler beim Abrufen der Ingress für Cluster '${CLSTRNM}'"
+        log "ERROR" "Error fetching ingress for cluster '${CLSTRNM}'"
         debug_crawler_error
         exit 1
+    else
+        log "INFO" "Ingress data for cluster '${CLSTRNM}' saved successfully"
     fi
 done < "${NAMEID_MAP}"
 
-# Git-Token und Repository konfigurieren  
+# Show contents of the info cache directory
+log "INFO" "Contents of directory ${INFO_CACHE}:"
+ls -l "${INFO_CACHE}"
+
+# Git token and repository configuration
 GIT_REPO_URL="https://gitlab-ci-token:${PUSH_BOM_PAGES}@git.f-i-ts.de/devops-services/toolchain/docs.git"
 
-
-# Klonen des Repositories in ein temporäres Verzeichnis
+# Clone the repository into a temporary directory
 REPO_DIR="/tmp/docs-repo"
 if [ ! -d "${REPO_DIR}" ]; then
     git clone "${GIT_REPO_URL}" "${REPO_DIR}"
 fi
 
-# Pfade zum Repository festlegen
+# Set paths within the repository
 INGRESS_PATH="${REPO_DIR}/boms/k8s/ingress"
 PODS_PATH="${REPO_DIR}/boms/k8s/pods"
 
-# Erstellen der Zielverzeichnisse im Repository
+# Create target directories in the repository
 create_directory "${INGRESS_PATH}"
 create_directory "${PODS_PATH}"
 
-# Kopiere die neuen Daten ins Repository-Verzeichnis
-cp -r ${INFO_CACHE}/*_ingress.json "${INGRESS_PATH}/"
-cp -r ${INFO_CACHE}/*_pods.json "${PODS_PATH}/"
+# Copy the new data into the repository directory
+if ! cp -r ${INFO_CACHE}/*_ingress.json "${INGRESS_PATH}/"; then
+    log "ERROR" "Error copying ingress files"
+    exit 1
+else
+    log "INFO" "Ingress files copied successfully"
+fi
 
-# Automatischer Commit und Push
+if ! cp -r ${INFO_CACHE}/*_pods.json "${PODS_PATH}/"; then
+    log "ERROR" "Error copying pods files"
+    exit 1
+else
+    log "INFO" "Pods files copied successfully"
+fi
+
+# Configure Git with a generic user
+git config --global user.email "ci-bot@example.com"
+git config --global user.name "CI Bot"
+
+# Automate commit and push
 cd "${REPO_DIR}" || exit
 git add .
-git commit -m "Automatisches Update der Cluster-Daten am $(date)"
+git commit -m "Automated update of cluster data on $(date)"
 git push "${GIT_REPO_URL}" main
